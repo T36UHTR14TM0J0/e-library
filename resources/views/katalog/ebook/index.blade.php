9f0197ab3f1763f433a9294386cd1b859d173647
teguh triatmojo <49945200+T36UHTR14TM0J0@users.noreply.github.com>
2025-06-12 00:25:42 +0700

    update migrate

--- resources/views/katalog/ebook/index.blade.php
+++ resources/views/katalog/ebook/index.blade.php
@@ -27,14 +27,26 @@
                 <div class="col-md-4">
                     <div class="card card-hover h-100">
                         <div class="card-header p-0 mx-3 mt-3 position-relative z-index-1">
+                            @php
+                                $coverImage = $ebook->gambar_sampul && Storage::exists($ebook->gambar_sampul) 
+                                            ? asset('storage/' . $ebook->gambar_sampul) 
+                                            : asset('assets/images/default-cover.png');
+                            @endphp
+                            
                             <div class="d-block blur-shadow-image">
-                                <img src="{{ asset('storage/' . $ebook->gambar_sampul) ?? asset('assets/images/default-cover.png') }}" 
-                                    alt="{{ $ebook->judul }}" 
+                                <img src="{{ $coverImage }}" 
+                                    alt="Cover {{ $ebook->judul }}" 
                                     class="img-fluid border-radius-lg" 
-                                    style="height: 200px; width: 100%; object-fit: cover;">
+                                    style="height: 200px; width: 100%; object-fit: cover;"
+                                    onerror="this.src='{{ asset('assets/images/default-cover.png') }}'">
                             </div>
+                            
                             <div class="colored-shadow" 
-                                style="background-image: url('{{ asset('storage/' . $ebook->gambar_sampul) ?? asset('assets/images/default-cover.png') }}');"></div>
+                                style="background-image: url('{{ $coverImage }}');
+                                        background-size: cover;
+                                        background-position: center;
+                                        background-repeat: no-repeat;">
+                            </div>
                         </div>
                         <div class="card-body pt-2">
                             <div class="d-flex justify-content-between align-items-center mb-2">
